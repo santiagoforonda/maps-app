94c051229860a0e54453ba196305ccb43a0b0e31
santiago <sergioforonda1894@gmail.com>
2026-05-28 11:50:37 -0500

    ultimas configuraciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
 import { MarkersPage } from './pages/markers-page/markers-page';
+import { HousesPage } from './pages/houses-page/houses-page';
 
 export const routes: Routes = [
 
@@ -16,7 +17,7 @@ export const routes: Routes = [
   },
   {
     path:"houses",
-    component:FullscreenMapPage,
+    component:HousesPage,
     title:"Casas - Propiedades"
   },
   {
